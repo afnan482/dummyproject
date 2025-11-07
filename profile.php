e25e314fd9f78bb40dae87d23ebe64a36faa5c25
paul <shivagembrut87@gmail.com>
2025-11-07 16:07:58 +0700

    Update code

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 
 <div class="container center-align" style="margin-top: 120px;">
   <i class="material-icons large" style="color: gray;">lock</i>
-  <h5>Silakan login dulu untuk melihat profil</h5>
+  <h5>Silakan logout/h5>
   <a href="login.php" class="btn blue waves-effect waves-light" style="margin-top: 20px;">
     <i class="material-icons left">login</i> Login
   </a>
